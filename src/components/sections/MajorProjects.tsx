472a915f489d2b65fdd1db6e963ba3e0d227642a
don9wan <dongwan1999@gmail.com>
2025-12-14 22:08:58 +0900

    Fix: 주요 시공경력 이미지 교체

--- src/components/sections/MajorProjects.tsx
+++ src/components/sections/MajorProjects.tsx
@@ -7,64 +7,76 @@ const MajorProjects = () => {
 
   const projects = [
     {
-      title: '인터불고호텔 버진로드',
-      year: "2022",
-  imageUrl: '/images/experience/experience_1.jpg',
-      imageAlt: '인터불고호텔 버진로드 조경 프로젝트'
+      title: '대구마이스터고 옥상 텃밭',
+      year: "2014",
+      imageUrl: '/images/experience/EX1.png',
+      imageAlt: '대구마이스터고 옥상 텃밭 조성 프로젝트'
     },
     {
-      title: '장기동 720M구간 산책로',
-      year: "2022",
-  imageUrl: '/images/experience/experience_2.jpg',
-      imageAlt: '장기동 720M구간 산책로 조성 프로젝트'
+      title: '도시농업박람회 미니도서관',
+      year: "2014",
+      imageUrl: '/images/experience/EX2.png',
+      imageAlt: '도시농업박람회 미니도서관 조성 프로젝트'
     },
     {
-      title: '동촌네거리 녹색쉼터',
-      year: '2021',
-  imageUrl: '/images/experience/experience_3.jpg',
+      title: '상도코아루 센트럴하임',
+      year: '2015',
+      imageUrl: '/images/experience/EX3.png',
+      imageAlt: '상도코아루 센트럴하임 조경 프로젝트'
+    },
+    {
+      title: '유림노르웨이숲 석가산',
+      year: '2017',
+      imageUrl: '/images/experience/EX4.png',
       imageAlt: '유림노르웨이숲 석가산 조경 프로젝트'
-    },
-    {
-      title: '환경연수원 모스인테리어',
-      year: '2022',
-  imageUrl: '/images/experience/experience_4.jpg',
-      imageAlt: '도시농업박람회 미니 도서관 조성 프로젝트'
     },
     {
       title: '신매 어린이공원 물놀이장',
       year: "2020",
-  imageUrl: '/images/experience/experience_5.jpg',
-      imageAlt: '동촌네거리 녹색쉼터 조성 프로젝트'
-    },
-    {
-      title: '만촌동 주택 담장',
-      year: "",
-  imageUrl: '/images/experience/experience_6.jpg',
-      imageAlt: '환경연수원 무소인테리어 프로젝트'
-    },
-    {
-      title: '유림노르웨이숲 석가산',
-      year: '',
-  imageUrl: '/images/experience/experience_7.jpg',
-      imageAlt: '대구마이스터고 옥상 텃밭 조성 프로젝트'
-    },
-    {
-      title: '도시농업박람회 미니 도서관',
-      year: '',
-  imageUrl: '/images/experience/experience_8.jpg',
-      imageAlt: '청아람 바닥분수 설치 프로젝트'
-    },
-    {
-      title: '대구마이스터고 옥상 텃밭',
-      year: "",
-  imageUrl: '/images/experience/experience_9.jpg',
+      imageUrl: '/images/experience/EX5.png',
       imageAlt: '신매 어린이공원 물놀이장 조성 프로젝트'
     },
     {
-      title: '청아람 바닥분수',
-      year: "",
-  imageUrl: '/images/experience/experience_10.jpg',
-      imageAlt: '만촌동 주택 담장 조경 프로젝트'
+      title: '만촌동 개인주택 담장',
+      year: "2020",
+      imageUrl: '/images/experience/EX6.png',
+      imageAlt: '만촌동 개인주택 담장 조경 프로젝트'
+    },
+    {
+      title: '동촌네거리 녹색쉼터 조성',
+      year: '2021',
+      imageUrl: '/images/experience/EX7.png',
+      imageAlt: '동촌네거리 녹색쉼터 조성 프로젝트'
+    },
+    {
+      title: '경북환경연수원 모스인테리어',
+      year: '2022',
+      imageUrl: '/images/experience/EX8.png',
+      imageAlt: '경북환경연수원 모스인테리어 프로젝트'
+    },
+    {
+      title: '장기동 완충녹지 720m 산책로',
+      year: "2022",
+      imageUrl: '/images/experience/EX9.png',
+      imageAlt: '장기동 완충녹지 720m 산책로 조성 프로젝트'
+    },
+    {
+      title: '공항교 꽃길 조성 공사',
+      year: "2023",
+      imageUrl: '/images/experience/EX10.png',
+      imageAlt: '공항교 꽃길 조성 공사 프로젝트'
+    },
+    {
+      title: '인터불고엑스코 그랑파티오 버진로드',
+      year: "2023",
+      imageUrl: '/images/experience/EX11.png',
+      imageAlt: '인터불고엑스코 그랑파티오 버진로드 조경 프로젝트'
+    },
+    {
+      title: '계명문화대 개교60주년기념 광장개선',
+      year: "2025",
+      imageUrl: '/images/experience/EX12.png',
+      imageAlt: '계명문화대 개교60주년기념 광장개선 프로젝트'
     }
   ];
 
